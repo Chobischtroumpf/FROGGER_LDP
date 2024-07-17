29e1a9e7652a6364fe16abfb19ce75e4a31630a9
Heraghon <gabriel.goldsztajn@gmail.com>
2024-07-17 13:02:57 +0300

    Edited main file to use mvc stack and run gameloop

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -12,8 +12,45 @@
 
 #include "MainWindow.hpp"
 
-int main(int argc, char *argv[]) {
-  MainWindow window(650, 650, "Frogger Game", 60);
-  window.show(argc, argv);
-  return Fl::run();
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include "GameModel.hpp"
+#include "GameView.hpp"
+#include "GameController.hpp"
+#include <iostream>
+
+void gameLoopCallback(void* controller_ptr) {
+    GameController* controller = static_cast<GameController*>(controller_ptr);
+    
+    controller->gameLoop();
+
+    // Repeat the gameloop every 1 / 60th of a second
+    Fl::repeat_timeout(1.0 / 60, gameLoopCallback, controller_ptr);
 }
+
+int main(int argc, char** argv) {
+    // initialise mvc instances
+    
+    std::cout << "Initialising model" << std::endl;
+
+    GameModel model;
+    std::cout << "Initialising Controller" << std::endl;
+    
+    GameView* view = nullptr;
+    GameController controller(&model, view);
+
+    std::cout << "Initialising View" << std::endl;
+    view = new GameView(1000, 1000, &model, &controller);
+
+    controller.linkView(view); // Set the view in the controller
+
+    std::cout << "Displaying view" << std::endl;
+    // Display the view
+    view->show(argc, argv);
+
+    // Game loop using FLTK timeout
+    Fl::add_timeout(1.0 / 60, gameLoopCallback, &controller);
+
+    return Fl::run();
+}
+
