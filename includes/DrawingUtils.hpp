3ad69597ac9b77391d7c52143eed94cb094cb5ab
Heraghon <gabriel.goldsztajn@gmail.com>
2024-08-01 19:30:44 +0200

    Added drawVehicle function

--- includes/DrawingUtils.hpp
+++ includes/DrawingUtils.hpp
@@ -8,6 +8,7 @@
 // Declare the functions
 void drawFilledRect(int x, int y, int width, int height, Fl_Color fillColor, Fl_Color frameColor = FL_BLACK);
 void drawSquare(Position pos, int size, Fl_Color fillColor = FL_WHITE, Fl_Color frameColor = FL_BLACK);
+void drawVehicle(Vehicle v);
 
 #endif // DRAWINGUTILS_HPP
 
--- srcs/DrawingUtils.cpp
+++ srcs/DrawingUtils.cpp
@@ -1,5 +1,22 @@
 #include "DrawingUtils.hpp"
 
+int tileSize = 50; // To be replaced with a dynamic value (no global variables)
+
+Fl_Color getVehicleColor(VehicleType type){
+    switch(type) {
+        case VehicleType::Bus :
+            return FL_YELLOW;
+        case VehicleType::Car:
+            return FL_RED;  
+        case VehicleType::Log:
+            return fl_rgb_color(55, 33, 0); // Brown
+        case VehicleType::Turtle:
+            return FL_DARK_GREEN;
+        default:
+            return FL_WHITE;
+    };
+}
+
 // Draw a filled rectangle with a frame
 void drawFilledRect(int x, int y, int width, int height, Fl_Color fillColor, Fl_Color frameColor ) {
     fl_color(fillColor);
@@ -11,8 +28,6 @@ void drawFilledRect(int x, int y, int width, int height, Fl_Color fillColor, Fl_
 
 void drawSquare(Position pos, int size, Fl_Color fillColor , Fl_Color frameColor ){
     
-    int tileSize = 50;
-
     // Set the fill color and draw the filled square
     fl_color(fillColor);
     fl_rectf(pos.x * tileSize + ( tileSize - size ) / 2 , pos.y * tileSize + ( tileSize - size ) / 2, size, size);
@@ -21,3 +36,43 @@ void drawSquare(Position pos, int size, Fl_Color fillColor , Fl_Color frameColor
     fl_color(frameColor);
     fl_rect(pos.x * tileSize + ( tileSize - size ) / 2, pos.y * tileSize + ( tileSize - size ) / 2, size, size);
 }
+
+// Draws a vehicle part on the board
+void drawVehicle(Vehicle v) {
+    int size = 30;
+
+    Fl_Color vehicleColor = getVehicleColor(v.getType());
+    fl_color(vehicleColor);
+
+    switch(v.getPart()) {
+        case VehiclePart::Front:
+            // Draw the front part of the vehicle
+            if (v.getDirection() == Direction::Right) {
+                fl_rectf(v.position.x * tileSize, v.position.y * tileSize + ( tileSize - size ) / 2, ( tileSize + size ) / 2, size);
+            } else {
+                fl_rectf(v.position.x * tileSize + ( tileSize - size ) / 2, v.position.y * tileSize + ( tileSize - size ) / 2, (tileSize + size) / 2, size);
+
+            }
+            break;
+        case VehiclePart::Center:
+            // Set the fill color and draw the middle part of the vehicle
+            fl_rectf(v.position.x * tileSize, v.position.y * tileSize + ( tileSize - size ) / 2, tileSize, size);
+            break;
+        case VehiclePart::End:
+            if (v.getDirection() == Direction::Right) {
+                fl_rectf(v.position.x * tileSize + ( tileSize - size ) / 2, v.position.y * tileSize + ( tileSize - size ) / 2, (tileSize + size) / 2, size);
+            } else {
+                fl_rectf(v.position.x * tileSize, v.position.y * tileSize + ( tileSize - size ) / 2, ( tileSize + size ) / 2, size);
+            }
+            break;
+        case VehiclePart::Single:
+            // Set the fill color and draw the single part of the vehicle
+            fl_rectf(v.position.x * tileSize + ( tileSize - size ) / 2 , v.position.y * tileSize + ( tileSize - size ) / 2, size, size);
+            break;
+        default:
+            break;
+    }
+}
+
+
+
